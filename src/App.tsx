3dc8f6556e7cc1eaaa4ca9b236ebba9a71fc3b82
technetfinanceiro-sys <technetfinanceiro960@gmail.com>
2026-05-04 11:48:12 -0300

    Corrige rotas para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Login from "./pages/Login";
@@ -19,7 +19,7 @@ const App = () => (
         <Toaster />
         <Sonner />
 
-        <BrowserRouter basename="/">
+        <HashRouter basename="/">
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route
@@ -48,7 +48,7 @@ const App = () => (
             />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
